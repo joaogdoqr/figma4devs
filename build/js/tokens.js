bd5eb61c87fe9759bd91ad7cc749d30406ea439c
João Pedro Giacometti Guerreiro <joao.guerreiro@doqr.com.br>
2024-10-29 07:46:36 -0300

    initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+module.exports = {
+  "ColorZinc100": "#f4f4f5",
+  "ColorZinc300": "#d4d4d8",
+  "ColorZinc500": "#71717a",
+  "ColorZinc800": "#27272a",
+  "ColorZinc950": "#09090b",
+  "ColorViolet500": "#8b5cf6"
+};
